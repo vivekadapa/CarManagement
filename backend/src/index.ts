32e1186cd4cb91eb5ffb4863000f721ea169a243
vivekadapa <vivekchaitanya1324@gmail.com>
2024-11-15 15:26:15 +0530

    add port to env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,6 +27,6 @@ app.use('/api/cars', carRoutes);
 app.use('/api/user', userRoutes)
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 }); 
